38f4d2bbbf242fbc0461484a8994fa4d58bb27ce
mrdistance <callmejoshcall@gmail.com>
2022-02-22 15:58:52 -0700

    Maven Dependencies Removed (I think '-')

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.inventorymanagementsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.inventorymanagementsystem to javafx.fxml;
-    exports com.example.inventorymanagementsystem;
-}
